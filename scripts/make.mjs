ae59c22a28b361ff8851d433adbd0c6c4026fdd3
Zakaria Lounes <zakaria.lounes@gmail.com>
2024-06-17 15:27:21 +0200

    chore: clean

--- scripts/make.mjs
+++ scripts/make.mjs
@@ -1,4 +1,4 @@
-import { default as writeConfig } from "./utils/useWriteConfig.mjs";
+import { default as writeFile } from "./utils/useWriteFile.mjs";
 import { default as protocolList } from "./utils/useProtocolList.mjs";
 
-writeConfig(JSON.stringify(protocolList()), "./build/protocols.json");
+writeFile(JSON.stringify(protocolList()), "./build/protocols.json");
